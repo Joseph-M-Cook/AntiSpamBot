eb55d5522055b098728174cbc5dc2ea7457a32e2
Joseph Cook <110889269+Joseph-M-Cook@users.noreply.github.com>
2023-09-27 12:12:11 -0400

    Update handler.py

--- handler.py
+++ handler.py
@@ -5,6 +5,9 @@ import os
 import requests
 import random
 import json
+import requests
+from bs4 import BeautifulSoup
+import re    # regex
 
 
 API_ROOT = 'https://api.groupme.com/v3/'
@@ -48,17 +51,53 @@ def kick_user(group_id, user_id, token):
     remove_member(group_id, membership_id, token)
 
 
+def extract_urls(message):
+    url_pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    return re.findall(url_pattern, message)
+
+
+def fetch_and_process_url(url):
+    for url in urls:
+        try:
+            response = requests.get(url, timeout=10)  
+            print(f"URL: {url} -> Status Code: {response.status_code}")
+
+            return response
+            
+        except requests.RequestException as e:
+            print(f"Error fetching URL: {url}. Error: {e}")
+            
+            return -1
+
+def flagged(message, bot_id):
+    kick_user(message['group_id'], message['user_id'], message['token'])
+    delete_message(message['group_id'], message['id'], message['token'])
+    send('Kicked ' + message['name'] + ' due to apparent spam post.', bot_id)
+
+
 def receive(event, context):
     message = json.loads(event['body'])
-
+    urls = extract_urls(message)
+    
     bot_id = message['bot_id']
+    # Process the original message
     for phrase in FLAGGED_PHRASES:
         if phrase in message['text'].lower():
-            kick_user(message['group_id'], message['user_id'], message['token'])
-            delete_message(message['group_id'], message['id'], message['token'])
-            send('Kicked ' + message['name'] + ' due to apparent spam post.', bot_id)
+            flagged(message, bot_id)
             break
 
+    # Check the URL/s
+    for url in urls:
+        response = fetch_and_process_url(url)
+        soup = BeautifulSoup(response.text)
+        try:
+            title = soup.find("meta",  property="og:title")
+            description = soup.find("meta",  property="og:description")
+            for phrase in FLAGGED_PHRASES:
+                if phrase in description.lower() or phrase in title.lower():
+                    flagged(message, bot_id)
+                    break
+                    
     return {
         'statusCode': 200,
         'body': 'ok'
